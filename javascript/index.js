3a94f1d54bfc3e4c020c53cac9c7e9039c9e17fa
Lo Hedengran <lohedengran@gmail.com>
2020-05-01 11:08:45 +0200

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+$(".container__location").on("click", () => {
+  $(".container__location-text").toggle();
+});
+
+$(".container__music").on("click", () => {
+  $(".container__music-text").toggle();
+});
+
+$(".container__notes").on("click", () => {
+  $(".container__notes-text").toggle();
+});
+
+$(".container__books").on("click", () => {
+  $(".container__books-text").toggle();
+});
+
+$(".container__tendances").on("click", () => {
+  $(".container__tendances-text").toggle();
+});
